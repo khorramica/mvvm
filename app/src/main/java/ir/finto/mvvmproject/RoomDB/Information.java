8372cc4359c957ab58e9fc4da23889c8273d0ee8
khorramica <84560576+khorramica@users.noreply.github.com>
2022-05-30 17:13:13 +0430

    Room Part01

--- app/src/main/java/ir/finto/mvvmproject/RoomDB/Information.java
+++ app/src/main/java/ir/finto/mvvmproject/RoomDB/Information.java
@@ -1,4 +1,40 @@
 package ir.finto.mvvmproject.RoomDB;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Information")
 public class Information {
+
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    private String id;
+
+    @ColumnInfo(name = "username")
+    private String username;
+
+    public Information(String id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public Information() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
---
+++ app/src/main/java/ir/finto/mvvmproject/RoomDB/InformationDao.java
@@ -0,0 +1,8 @@
+package ir.finto.mvvmproject.RoomDB;
+
+import androidx.room.Dao;
+
+@Dao
+public interface InformationDao {
+
+}
